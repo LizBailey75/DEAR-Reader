95fb792af9e38d0d2398e20f2958c92f9669c68c
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-11-10 13:07:34 -0700

    links in navigation working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -94,6 +94,26 @@ router.get('/allReviews', withAuth, async (req, res) => {
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/createReview', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('createReview', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
