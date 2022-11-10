96b3a1bcfcd333b55786064384ee13191edc85b0
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-11-10 11:36:25 -0700

    altered CSS

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -74,6 +74,26 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/allReviews', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('allReviews', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -83,5 +103,7 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+
+
 
 module.exports = router;
